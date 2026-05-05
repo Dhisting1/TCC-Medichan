8d7a0a04e90c1d8135d0f37e9845e4ab003d33ec
Dhisting1 <lucas.lima@estudante.ifb.edu.br>
2026-05-05 11:22:32 -0300

    uo

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 export const redis = createClient({
-  url: "redis://localhost:6379",
+  url: "redis://default:senha@redis.railway.internal:6379",
 });
 
 redis.connect();
