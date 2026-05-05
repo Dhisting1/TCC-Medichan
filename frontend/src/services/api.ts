07ed7f51f1a90bc2f8a5b93189f15adc37408ee3
Dhisting1 <lucas.lima@estudante.ifb.edu.br>
2026-05-05 13:13:06 -0300

    teste

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://tcc-medichan-production.up.railway.app/",
 });
 
 // injeta o token automaticamente em todas as requisições
